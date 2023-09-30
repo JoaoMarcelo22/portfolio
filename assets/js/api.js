e52d75846d1a889fb4606d1a00cd8b3bdae81bae
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-09-30 01:17:43 -0300

    feat: adicionando json na jogada

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/JoaoMarcelo22/portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
