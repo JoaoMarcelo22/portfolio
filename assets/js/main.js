8d09d82a0da9ef7436827be4d0e68da689a0fed1
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-10-03 22:30:43 -0300

    feat: finalizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,19 +33,6 @@ function updateProfileInfo(profileData) {
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
 
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
-
-    const location = document.getElementById('profile.location')
-    location.innerText = profileData.location
-
-    const phone = document.getElementById('profile.phone')
-    phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
-
-    const email = document.getElementById('profile.email')
-    email.innerText = profileData.email
-    email.href = `mailto:${profileData.email}`
 }
 
 function updateSoftSkills(profileData) {
